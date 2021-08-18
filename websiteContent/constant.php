58bba9a8ef766d07c43b184ee3b49da7ae522f67
sidh261805 <33148301+sidh261805@users.noreply.github.com>
2021-08-18 13:29:13 +0200

    Update constant.php

--- websiteContent/constant.php
+++ websiteContent/constant.php
@@ -1,5 +1,5 @@
 <?php
 //reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-define('SITE_KEY',"6LelMI0UAAAAANMTI6p0KPN88K67OlCt9tgQuTIc"); 
-define('SECRET_KEY',"6LelMI0UAAAAAO7xz3GRvZZMpSNkbMAnOavEGnHd");
+define('SITE_KEY',"6LfPlAscAAAAAExlRw6LFif0oxsTV2CYuvzgsN2c"); 
+define('SECRET_KEY',"6LfPlAscAAAAANuQJQ6AkYk2YvS0i9MQi_YQQ9eW");
 ?>
