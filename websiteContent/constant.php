2a150da4cb826a1f23842a7200128abdfc181fb0
sidh261805 <33148301+sidh261805@users.noreply.github.com>
2021-08-18 16:36:31 +0200

    Delete constant.php

--- websiteContent/constant.php
+++
@@ -1,5 +0,0 @@
-<?php
-//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-define('SITE_KEY',"6LdklwscAAAAAMrtBYotYD__fiLmIYC6Ni_-Iz7x"); 
-define('SECRET_KEY',"6LdklwscAAAAABl-WY-H7deS0csrTU4F57rMpFWP");
-?>
