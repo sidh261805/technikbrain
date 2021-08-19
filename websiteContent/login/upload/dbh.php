305eedb53c9f208e7a1d486cd8f68c99faac75fa
sidh261805 <33148301+sidh261805@users.noreply.github.com>
2021-08-19 18:58:20 +0200

    Update dbh.php

--- websiteContent/login/upload/dbh.php
+++ websiteContent/login/upload/dbh.php
@@ -1,6 +1,6 @@
 <?php
 //to connect database server
-include '../../../../passwords/db_access.php';
+include '../../../passwords/db_access.php';
 $conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
 if (!$conn) {
    die("Connection failed: " . mysqli_connect_error());
