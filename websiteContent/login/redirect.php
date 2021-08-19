a9b158b4695efcaf6fa1586f03946167313fd81a
sidh261805 <33148301+sidh261805@users.noreply.github.com>
2021-08-19 11:47:39 +0200

    Update redirect.php

--- websiteContent/login/redirect.php
+++ websiteContent/login/redirect.php
@@ -16,10 +16,10 @@
 		$username = $con->real_escape_string($_GET['username']);
 		$token = $con->real_escape_string($_GET['token']);
 		
-		$sql = $con->query(query: "SELECT id FROM users WHERE username='$username' AND token='$token' AND isEmailConfirmed=0");
+		$sql = $con->query("SELECT id FROM users WHERE username='$username' AND token='$token' AND isEmailConfirmed=0");
 		
 		if ($sql->num_rows > 0){
-			$con->query(query: "UPDATE users SET isEmailConfirmed=1, token='' WHERE username='$username'");
+			$con->query("UPDATE users SET isEmailConfirmed=1, token='' WHERE username='$username'");
 			echo 'Your username has been verified! You can log in now!';
 		} else {
 			echo 'end';
