d42d2688b321e180bccb6d4aa7fb21ee7a80fc9d
sidh261805 <33148301+sidh261805@users.noreply.github.com>
2021-08-23 22:29:52 +0200

    Update index.php

--- websiteContent/login/index.php
+++ websiteContent/login/index.php
@@ -48,7 +48,7 @@
 					if ($row = mysqli_fetch_assoc($result))
 						{
 							$imagename = $row['imagename'];
-							echo "<img src='upload/".$imagename."'><br>";
+							echo "<img src='upload/".$imagename."' height='130' width='150'><br>";
 						}		
 				} else {
 					 echo "<img src='../../images/default.png'><br>";
